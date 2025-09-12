19b9353f2a63790d8bdcf47bb4b7250cc96692c2
Gustavo R. Vargas <ing.gustavo.rv@gmail.com>
2025-09-12 17:12:05 -0600

    update p022

--- Elemetnos-Basicos/p022-resistencia-equivalente-paralelo.py
+++ Elemetnos-Basicos/p022-resistencia-equivalente-paralelo.py
@@ -12,9 +12,10 @@ print("-" * 40)
 r1 = float(input('Ingrese el valor de la resistencia R1: '))
 r2 = float(input('Ingrese el valor de la resistencia R2: '))
 r3 = float(input('Ingrese el valor de la resistencia R3: '))
+r4 = float(input('Ingrese el valor de la resistencia R4: '))
 
 # Proceso
-rt = 1 / ( (1/r1) + (1/r2) + (1/r3) )
+rt = 1 / ( (1/r1) + (1/r2) + (1/r3) + (1/r4) )
 
 # Salida
 print("-" * 40)
