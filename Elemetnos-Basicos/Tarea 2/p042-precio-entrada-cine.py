de5d508de960a39689fd2325a3ccf7351e14e23c
Gustavo R. Vargas <ing.gustavo.rv@gmail.com>
2025-09-24 17:49:41 -0600

    Tarea 2

---
+++ Elemetnos-Basicos/Tarea 2/p042-precio-entrada-cine.py
@@ -0,0 +1,34 @@
+# p042-precio-entrada-cine.py
+# Problema: Crea un programa para la taquilla de un cine que determine el precio de una entrada según la edad del
+# cliente. El programa debe solicitar la edad y mostrar el precio correspondiente, siguiendo estas reglas:
+# • Menores de 5 años: Entran gratis.
+# • Niños (5 a 12 años): Pagan $5.
+# • Adultos (13 a 64 años): Pagan $10.
+# • Tercera edad (65 años o más): Pagan $7.
+# Ejemplos de ejecución:
+# • Entrada: Edad: 4
+# • Salida: Tu entrada es gratis.
+# • Entrada: Edad: 10
+# • Salida: El precio de tu entrada es de $5.
+# • Entrada: Edad: 35
+# • Salida: El precio de tu entrada es de $10.
+
+print('\033[2J\033[H')
+print('--- Verificar precio de entrada ---')
+
+edad = int(input('Dame tu edad: '))
+
+if edad < 5: 
+    print('Tu entrada es gratis.')
+elif edad < 13:
+    print('El precio de tu entrada es de $5')
+elif edad < 65:
+    print('El precio de tu entrada es de $10')
+elif edad < 100:
+    print('El precio de tu entrada es de $7')
+else:
+    print('Ya te moriste')
+
+print('\nProceso temrinado ...')
+
+
---
+++ Elemetnos-Basicos/Tarea 2/p043-calculadora-anio-bisiesto.py
@@ -0,0 +1,36 @@
+# p043-calculadora-anio-bisiesto.py 
+# Problema: Escribe un programa que determine si un año, ingresado por el usuario, es bisiesto. Un año es bisiesto
+# si cumple una de las siguientes condiciones:
+# 1. Es divisible por 4, pero no es divisible por 100.
+# 2. Es divisible por 400.
+# El programa debe indicar claramente si el año es bisiesto o no.
+# Ejemplos de ejecución:
+# • Entrada: 2024
+# • Salida: El año 2024 es bisiesto. (Porque es divisible por 4 pero no por 100).
+# • Entrada: 1900
+# • Salida: El año 1900 no es bisiesto. (Porque es divisible por 100 pero no por 400).
+# • Entrada: 2000
+# • Salida: El año 2000 es bisiesto. (Porque es divisible por 400).
+# • Entrada: 2023
+# • Salida: El año 2023 no es bisiesto. (Porque no es divisible por 4).
+
+print('\033[2J\033[H')
+print('--- Verificar año bisiesto ---')
+
+año = int(input('Dame un año: '))
+
+if año % 400 == 0:
+    print(f'El año {año} es bisiesto. (Porque es divisible por 400).')
+elif año % 100 == 0:
+    print(f'El año {año} no es bisiesto. (Porque es divisible por 100 pero no por 400).')
+elif año % 4 == 0:
+    print(f'El año {año} es bisiesto. (Porque es divisible por 4 pero no por 100).')
+else:
+    print(f'El año {año} no es bisiesto. (Porque no es divisible por 4).')
+    
+print('\nProceso terminado ...')
+
+
+
+
+
