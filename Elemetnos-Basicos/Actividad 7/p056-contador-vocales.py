07b79d7e429c06f14ec9a436e27d3e0fdbad9dd4
Gustavo R. Vargas <ing.gustavo.rv@gmail.com>
2025-09-30 13:31:13 -0600

    Actividad 7

---
+++ Elemetnos-Basicos/Actividad 7/p056-contador-vocales.py
@@ -0,0 +1,34 @@
+# p056-contador-vocales.py
+# Cuenta vocales, consonantes y otros caracteres en una frase
+
+while True:
+    print('\033[2J\033[H')
+    print('Contador de vocales y consonantes')
+    frase = input('Introduce una frase : ')
+    
+    vocales = consonantes = otros = 0
+    indice = 0
+    
+    while indice < len(frase):
+        caracter = frase[indice] # Tomamos el caracter correspondiente de la frase
+        
+        if caracter >= 'a' and caracter <= 'z': # Verificamos que sea letra
+            if caracter in 'aeiou':
+                vocales += 1
+            else: # es consonatnes
+                consonantes += 1
+        else: # es cualqueir otra cosa
+            otros += 1
+            
+        indice += 1
+    
+    print(f'Análisis de la frase')
+    print(f'Número de vocales     : {vocales}')
+    print(f'Número de consonantes : {consonantes}')
+    print(f'Número de otros       : {otros}')
+    
+    if input('\n¿Deseas continuar (S/N)? ').upper()=='N': break
+    
+print('\nProceso terminado')
+
+
